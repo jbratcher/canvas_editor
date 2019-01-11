b08c590554d035a20ea1c7cd4bd39a55b51526b0
Jeremy Bratcher <jeremybratcher@gmail.com>
2019-01-11 12:51:43 +0000

    adjusting canvas to dynamically match window size, scaled drawSquare func to match window size

--- src/Canvas.js
+++ src/Canvas.js
@@ -5,26 +5,37 @@ class Canvas extends Component {
     
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+          height: window.innerHeight,
+          width: window.innerWidth
+        };
     }
     
     drawSquare = () => {
-    
+      const {height, width} = this.state;
       const ctx = this.refs.canvas.getContext('2d');
+      this.updateCanvas();
       ctx.fillStyle = "blue";
-      ctx.fillRect(0,0, 10, 100);
-      
+      ctx.fillRect(width*0.125, height*0.125, width*0.25, height*0.25);
+      console.log("Square drawn at:", "\nx1: ", width*0.125, "\ny1: ", height*0.125, "\nx2: ", width*0.25, "\ny2: ", height*0.25);
     }
     
+    // startMouseDrag = () => {
+    //   null;
+    // }
     
    componentDidMount() {
         this.updateCanvas();
     }
     
     updateCanvas() {
-        const ctx = this.refs.canvas.getContext('2d');
-        ctx.fillStyle = "#eee";
-        ctx.fillRect(0,0, 300, 300);
+      const {height, width} = this.state;
+      const ctx = this.refs.canvas.getContext('2d');
+      ctx.canvas.width  = window.innerWidth;
+      ctx.canvas.height = window.innerHeight;
+      ctx.fillStyle = "#eee";
+      ctx.fillRect(0,0, width, height);
+      console.log("Browser size: \n", "Width:", width, "Height", height);
     }
     
   render() {
@@ -34,7 +45,7 @@ class Canvas extends Component {
       
         <button className="drawSquare" onClick={this.drawSquare}>Draw Square</button>
         
-        <canvas ref="canvas" height="800" width="100%">
+        <canvas onClick={this.startMouseDrag} ref="canvas" height="800" width="100%">
           Canvas (Your browser doesn't support the canvas element).
         </canvas>
       
