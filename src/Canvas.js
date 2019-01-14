37a285d7f259fd3b9d3bd0ce50634bf87d9a88ca
Jeremy Bratcher <jeremybratcher@gmail.com>
2019-01-14 12:46:58 +0000

    feat: added circle and triangle buttons

--- src/Canvas.js
+++ src/Canvas.js
@@ -7,36 +7,44 @@ class Canvas extends Component {
         super(props);
         
         this.state = {
+          ctx: null,
           height: null,
           width: null
         };
     }
     
     drawSquare = () => {
-      const height = this.refs.canvasContainer.offsetHeight;
-      const width = this.refs.canvasContainer.offsetWidth;
-      const ctx = this.refs.canvas.getContext('2d');
+      const {ctx, height, width} = this.state;
       this.updateCanvas();
       ctx.fillStyle = "blue";
       ctx.fillRect(width*0.125, height*0.125, width*0.25, height*0.25);
+      ctx.stroke();
       console.log("Square drawn at:", "\nx1: ", width*0.125, "\ny1: ", height*0.125, "\nx2: ", width*0.25, "\ny2: ", height*0.25);
+      console.log(this.state);
     }
     
-    // startMouseDrag = () => {
-    //   null;
-    // }
-    
-    componentDidMount() {
-     
-      this.setState({
-        height: this.refs.canvasContainer.offsetHeight,
-        width: this.refs.canvasContainer.offsetWidth
-      });
-     
+    drawCircle = () => {
+      const {ctx} = this.state;
       this.updateCanvas();
-      
-      console.log("Canvas Component loaded state: ", this.state);
-      
+      ctx.beginPath();
+      ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.stroke();
+      console.log(this.state);
+    }
+    
+    drawTriangle = () => {
+      const {ctx} = this.state;
+      this.updateCanvas();
+      ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      ctx.fillStyle="red";
+      ctx.fill();
+      ctx.stroke();
+      console.log(this.state);
     }
     
     updateCanvas() {
@@ -50,18 +58,44 @@ class Canvas extends Component {
       console.log("Browser size: \n", "Width:", width, "Height", height);
     }
     
+    componentDidMount() {
+     
+      this.setState({
+        ctx: this.refs.canvas.getContext('2d'),
+        height: this.refs.canvasContainer.offsetHeight,
+        width: this.refs.canvasContainer.offsetWidth
+      });
+     
+      this.updateCanvas();
+      
+      console.log("Canvas Component loaded state: ", this.state);
+      
+    }
+    
   render() {
     return (
       
-      <section className="canvasContainer" ref="canvasContainer">
+      <React.Fragment>
       
-        <button className="drawSquare" onClick={this.drawSquare}>Draw Square</button>
+        <section className="buttonContainer ref=buttonContainer">
         
-        <canvas onClick={this.startMouseDrag} ref="canvas" height="100%" width="100%">
-          Canvas (Your browser doesn't support the canvas element).
-        </canvas>
+          <button className="drawSquare" onClick={this.drawSquare}>Draw Square</button>
+          
+          <button className="drawCircle" onClick={this.drawCircle}>Draw Circle</button>
+          
+          <button className="drawTriangle" onClick={this.drawTriangle}>Draw Triangle</button>
+        
+        </section>
+        
+        <section className="canvasContainer" ref="canvasContainer">
+          
+          <canvas onClick={this.startMouseDrag} ref="canvas" height="100%" width="100%">
+            Canvas (Your browser doesn't support the canvas element).
+          </canvas>
+        
+        </section>
       
-      </section>
+      </React.Fragment>
       
     );
   }
