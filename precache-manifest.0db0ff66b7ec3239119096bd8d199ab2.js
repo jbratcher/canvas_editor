990dcc4fa0436a069c03e3efea91b6b250653744
Jeremy Bratcher <jeremybratcher@gmail.com>
2019-02-02 17:35:43 +0000

    Updates

---
+++ precache-manifest.0db0ff66b7ec3239119096bd8d199ab2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8860404a11ceb46c626a",
+    "url": "/canvas_editor/static/js/runtime~main.8860404a.js"
+  },
+  {
+    "revision": "b748fa860f775ece966c",
+    "url": "/canvas_editor/static/js/main.b748fa86.chunk.js"
+  },
+  {
+    "revision": "046a9b4d0f1bb3ae2c4f",
+    "url": "/canvas_editor/static/js/1.046a9b4d.chunk.js"
+  },
+  {
+    "revision": "b748fa860f775ece966c",
+    "url": "/canvas_editor/static/css/main.e1fdcb04.chunk.css"
+  },
+  {
+    "revision": "0023c0f2c10317703ea9514e60861a74",
+    "url": "/canvas_editor/index.html"
+  }
+];
